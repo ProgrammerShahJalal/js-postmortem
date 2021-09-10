fb0474ea79380bfdd2fe5fe566a8c789c5dc4efe
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-09-10 10:13:32 +0600

    first commit

---
+++ postmortem.js
@@ -0,0 +1,14 @@
+function steveKaku() {
+    billGatesKaku()
+    console.log("I am steve jobs");
+}
+function billGatesKaku() {
+    markZuckerbergKaku()
+    console.log("I am bill gates");
+}
+function markZuckerbergKaku() {
+    console.log("I am mark zuckerberg");
+}
+const charlesbables = "I am togo bap charles bales";
+steveKaku();
+console.log(charlesbables);
